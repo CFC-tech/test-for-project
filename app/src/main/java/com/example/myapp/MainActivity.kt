94a7a8ed2d977165e40b2481ff4cff7dac22ae27
Admin <kyawsoelinn24@gmail.com>
2025-11-18 22:28:02 +0630

    setup project

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.myapp
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.myapp.databinding.ActivityMainBinding
 import kotlin.jvm.java
 
@@ -13,5 +16,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
     }
 }
